d5f91d097c21c722bcede4b3f289e250b002aa30
faisalkn-71 <csust2021@gmail.com>
2022-04-17 00:00:59 +0600

    services-done

---
+++ src/Pages/Home/Service/Service.js
@@ -0,0 +1,18 @@
+import React from 'react';
+const Service = ({service}) => {
+    const {name, price, description, picture} = service;
+    return (
+            
+            <div>
+                <img style={{height:'250px'}} className='w-100 rounded' src={picture} alt="" />
+                <h3 className='mt-2'>{name}</h3>
+                <p className='text-start'><small>{description}</small></p>
+                <p>Price: {price}</p>
+                <button className='btn btn-primary w-50'>Book Now</button>
+            </div>
+
+       
+    );
+};
+
+export default Service;
