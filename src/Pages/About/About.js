deefb85df691748378c41ea2aa2f3d40fe1b58d6
faisalkn-71 <csust2021@gmail.com>
2022-04-17 16:53:06 +0600

    about-section-done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import myself from '../../images/About/Myself.jpg'
 
 const About = () => {
     return (
-        <div>
-            <h2>wnat to know about me</h2>
+        <div className='App container mt-3'>
+            <img style={{width:'250px', height:'300px'}} src={myself} alt="" />
+            <h3 className='mt-2'>Md Faisal Karim</h3>
+            <p className='text-start'>I am a university student. I study at computer science and technology in Changsha University of Science and Technology, China. In the beginning of this year I started learning web-developing. Hopefully, I will a web-developer within this year.</p>
+            <p className='text-start'>My goals are learning web-developing, learning C++ programming language and doing atlest 200 problem solving via C++ programming language.</p>
+            <p className='text-start'>To achieve my goals I spend atleast 8 hours in a day. Because I know one thing which is there is no way to find our goals except hard work.</p>
+            <h5 className='text-bold text-primary'>Through hard work, perseverance and a faith in God, you can live your dreams.</h5>
         </div>
     );
 };
