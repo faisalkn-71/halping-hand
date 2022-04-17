e03c8c0a2f81afabc6fe2bf2f2e49eeee078e41d
faisalkn-71 <csust2021@gmail.com>
2022-04-17 12:36:43 +0600

    handle-log-out

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -3,6 +3,8 @@ import { Button, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import { ToastContainer, toast } from 'react-toastify';
+
 
 const Login = () => {
     const navigate = useNavigate();
@@ -15,18 +17,18 @@ const Login = () => {
         user,
         loading,
         error,
-      ] =  useSignInWithEmailAndPassword(auth);
+    ] = useSignInWithEmailAndPassword(auth);
 
-      const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
 
-      let errorElement;
-      if(error){
-          errorElement = <p className='text-danger'>Error: {error.message}</p>
-      }
+    let errorElement;
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error.message}</p>
+    }
 
-      if(user){
+    if (user) {
         navigate('/home')
-      }
+    }
 
     const handleLogin = (event) => {
         event.preventDefault();
@@ -35,27 +37,32 @@ const Login = () => {
         const password = event.target.password.value;
 
         signInWithEmailAndPassword(email, password)
-        
+
     }
 
-    const handleResetPassword = async(event) => {
+    const handleResetPassword = async (event) => {
         const email = event.target.email.value;
-        await sendPasswordResetEmail(email);
-        alert('Sent email');
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast('Sent email');
+        }
+        else {
+            toast('Please enter your email adress.')
+        }
     }
     return (
         <div className='container w-50 mx-atuo mt-5'>
             <h2 className='text-primary text-center mb-3'>Please Login</h2>
             <Form onSubmit={handleLogin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                   
-                    <Form.Control type="email" name='email' placeholder="Enter email" required/>
-                    
+
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    
-                    <Form.Control type="password" name='password' placeholder="Password" required/>
+
+                    <Form.Control type="password" name='password' placeholder="Password" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
@@ -65,8 +72,10 @@ const Login = () => {
                     Login
                 </Button>
             </Form>
-            <p className='mt-3'><span onClick={navigateRegister} style={{cursor: 'pointer'}} className='text-primary'>Forget Password?</span></p>
-            <p className='mt-3'>New to Helping Hand? <span onClick={navigateRegister} style={{cursor: 'pointer'}} className='text-primary'>Please Register</span></p>
+            <p className='mt-3'><span onClick={handleResetPassword} style={{ cursor: 'pointer' }} className='text-primary'>Forget Password?</span></p>
+            <p className='mt-3'>New to Helping Hand? <span onClick={navigateRegister} style={{ cursor: 'pointer' }} className='text-primary'>Please Register</span></p>
+            <ToastContainer />
+
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init'
+import { signOut } from 'firebase/auth';
+
 
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+    const handleLogout = () => {
+        signOut(auth)
+    }
     return (
         <Navbar collapseOnSelect expand="lg" sticky='top' bg="primary" variant="dark">
             <Container>
@@ -15,13 +23,17 @@ const Header = () => {
                     <Nav className="me-auto">
                         <Nav.Link href="/home#services">Services</Nav.Link>
                         <Nav.Link href="/home#reviews">Client Feedback</Nav.Link>
-                        
+
                     </Nav>
                     <Nav>
                         <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
                         <Nav.Link as={Link} to="/about">About</Nav.Link>
-                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                        
+                        {   user ?
+                            <button className='btn btn-primary' onClick={handleLogout}>LogOut</button> 
+                            :   
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                        }
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
